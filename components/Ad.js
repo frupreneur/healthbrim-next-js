6d8502b155318ebd3404bbd8658fd188d3e90c0e
frupreneur <frupreneur@gmail.com>
2023-01-30 02:53:47 +0100

    updated ads refresh on every route

--- components/Ad.js
+++ components/Ad.js
@@ -1,37 +1,45 @@
 import React from "react";
 export default function Ad() {
+  const [state, setState] = React.useState(true);
+
+  const adHolder = React.useRef();
   React.useEffect(() => {
+    adHolder.current.innerHTML = `<ins
+    class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-4787236575206177"
+    data-ad-slot="6071203613"
+    data-ad-format="auto"
+    data-full-width-responsive="true"
+  ></ins>`;
+
     let timer = setTimeout(() => {
       try {
         if (typeof window !== "undefined") {
-          (adsbygoogle = window.adsbygoogle || []).push({});
+          console
+            .log("rendering Ad component")(
+              (adsbygoogle = window.adsbygoogle || [])
+            )
+            .push({});
         }
       } catch (err) {
         console.error(err);
       }
     }, 3000);
     return () => {
+      console.log("unmounting Ad component");
       clearTimeout(timer);
     };
   });
 
-  const ins = (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-4787236575206177"
-      data-ad-slot="6071203613"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
-  );
-
   return (
     <>
-      <div id="ads" class="ad-container" aria-hidden={true}>
-        <p style={{ fontStyle: "italic" }}>Advertisement</p>
-        {ins}
-      </div>
+      {state && (
+        <div id="ads" className={`ad-container`} aria-hidden={true}>
+          <p style={{ fontStyle: "italic" }}>Advertisement</p>
+          <div className="adHolder" ref={adHolder}></div>
+        </div>
+      )}
     </>
   );
 }
